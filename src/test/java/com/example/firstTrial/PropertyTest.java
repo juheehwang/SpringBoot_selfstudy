4d714f8a50ed2e37bf7dab6b011d2f3a950487bd
juhee <fhqj21@gmail.com>
2021-08-22 17:26:58 +0900

    create POJO class

--- src/test/java/com/example/firstTrial/PropertyTest.java
+++ src/test/java/com/example/firstTrial/PropertyTest.java
@@ -1,10 +1,10 @@
 package com.example.firstTrial;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Map;
@@ -12,7 +12,7 @@ import java.util.Map;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PropertyTest {
 
@@ -21,11 +21,29 @@ public class PropertyTest {
 
     @Test
     public void test(){
-        List<Map> fruitData = fruitProperty.getList();
+//        List<Map> fruitData = fruitProperty.getList();
+        List<Fruit> fruitData = fruitProperty.getList();
 
-        assertThat(fruitData.get(0).get("name"), is("banana"));
-        assertThat(fruitData.get(0).get("color"), is("yellow"));
-        
-        assertThat(fruitData.get(0).get("name"), is("banana"));
+        // Map의 get()메서드로 값을 불러오기
+//        assertThat(fruitData.get(0).get("name"), is("banana"));
+//        assertThat(fruitData.get(0).get("color"), is("yellow"));
+//
+//        assertThat(fruitData.get(1).get("name"), is("apple"));
+//        assertThat(fruitData.get(1).get("color"), is("red"));
+//
+//        assertThat(fruitData.get(2).get("name"), is("water melon"));
+//        assertThat(fruitData.get(2).get("color"), is("green"));
+
+        // Fruit의 객체 메서드를 사용하여 값을 읽어오기
+        assertThat(fruitData.get(0).getName(), is("banana"));
+        assertThat(fruitData.get(0).getColor(), is("yellow"));
+
+        assertThat(fruitData.get(1).getName(), is("apple"));
+        assertThat(fruitData.get(1).getColor(), is("red"));
+
+        assertThat(fruitData.get(2).getName(), is("water melon"));
+        assertThat(fruitData.get(2).getColor(), is("green"));
+
+
     }
 }
